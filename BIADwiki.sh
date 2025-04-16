89706459c6ff233d1d1bf8b30882d02e01a04dad
AdrianTimpson <a.timpson@ucl.ac.uk>
2025-04-16 12:48:47 +0100

    Update BIADwiki.sh

--- BIADwiki.sh
+++ BIADwiki.sh
@@ -1,31 +1,34 @@
 #!/bin/sh
 PATH="$PATH:/usr/local/bin/"
 
-# git pull first
+# BIADwiki controller
+
+echo -e "Running BIADwiki controller"
+echo "$PWD"
+echo `date`
+ 
+# server pulls the latest from the github repository (including the latest version of this file)
+# server runs this file only
 git status
 git pull
 
 DATE=$(date +%Y%m%d)
-#
+
 # run R scripts
+echo -e "Starting BIADwiki/controller.R"
 cd R
-Rscript controller.R > controller.Rout_$DATE
-## this should be changed and use a symlink to the last, but this will need adjustmenet depending on the docker
-scp -P 2222 controller.Rout_$DATE tunnel@biad.cloud:/media/biad/controller_last.txt
-mv controller.Rout_$DATE $LOGS_FOLDER
+Rscript controller.R > BIADwiki.controller.Rout_$DATE
+cd ..
+echo -e "Ending BIADwiki/controller.R"
+
+# this should be changed and use a symlink to the last, but this will need adjustmenet depending on the docker
+scp -P 2222 BIADwiki.controller.Rout_$DATE tunnel@biad.cloud:/media/biad/BIADwiki.controller_last.txt
+mv BIADwiki.controller.Rout_$DATE $LOGS_FOLDER
 cd ..
 
-# backup files in dropplet scp tunnel
+# backup files in droplet scp tunnel
 for fold in templates summary_stats table_comments ;
 do
     rsync -avz -e "ssh -p 2222" tools/$fold tunnel@biad.cloud:/media/biad/
 done
-
-# one last pull, then push the changes
-git status
-git pull
-git add -A tools/table_comments/
-git add -A tools/templates/
-git commit -m "auto update from server"
-git push
-git status
+#-------------------------------------------------------------------------------------------
