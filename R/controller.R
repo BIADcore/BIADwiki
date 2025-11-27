7db5b1934d18ff94eb1c6d57647347491514ad06
AdrianTimpson <a.timpson@ucl.ac.uk>
2025-11-27 19:59:49 +0000

    Update controller.R

--- R/controller.R
+++ R/controller.R
@@ -1,15 +1,7 @@
 #------------------------------------------------------------------
 # Runs all R scripts beginning with 'auto.make.'
 #------------------------------------------------------------------
-# install BIADconnect if required
-if(!'BIADconnect'%in%installed.packages())install.packages("~/BIADconnect/", repos = NULL, type = "source")
-require(BIADconnect)
+library(BIADconnect)
 source('../../BIADadmin/R/functions.R')
-#------------------------------------------------------------------
-conn  <-  init.conn()
-cat(paste('BIAD check routine started on:\n',date(),'\n'))
-cat(paste(' - BIAD size is of:',round(getSize(conn = conn)[1,2]*1000),'Mo\n'))
-disconnect()
-
 run.scripts.in.this.folder(pattern='auto.make')
 #------------------------------------------------------------------
