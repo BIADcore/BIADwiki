b12065ff171e0fc7791c449d4fd79acbf6a8542c
AdrianTimpson <a.timpson@ucl.ac.uk>
2025-11-28 14:55:59 +0000

    timeslice maps

--- R/auto.make.104.plot.map.timeslices.R
+++ R/auto.make.104.plot.map.timeslices.R
@@ -14,9 +14,11 @@ require(maps)
 require(mapdata)
 require(svglite)
 require(BIADconnect)
-#--------------------------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------------------------
+
 conn <- init.conn()
+#--------------------------------------------------------------------------------------------------
+# get chronology
+#--------------------------------------------------------------------------------------------------
 sit <- query.database("SELECT * FROM `BIAD`.`Sites`", conn=conn)
 pha <- query.database("SELECT * FROM `BIAD`.`Phases`", conn=conn)
 d <- merge(pha,sit,by='SiteID')
@@ -29,46 +31,62 @@ d$date[i] <- d$reportedMidBP[i]
 # remove those still with no chronology
 d <- d[!is.na(d$date),]
 #--------------------------------------------------------------------------------------------------
-# design the universal limits
+# overheads
 #--------------------------------------------------------------------------------------------------
 xlim <- quantile(d$Longitude, prob=c(0.025,0.975))
 ylim <- quantile(d$Latitude, prob=c(0.025,0.975))
-N <- 10
-zposts <- round(rev(quantile(d$date, prob=seq(0,1,length.out=N+1))),-2)
 
 # plot sizes
 pheight <- 8
 pwidth <- pheight * round(diff(xlim)/diff(ylim),1)
+
+# paths
+folder <- '../tools/plots/'
+prefix <- 'timeslice.map'
 #--------------------------------------------------------------------------------------------------
-# common plot function
+# common functions. Can be moved to functions.R once well established + debugged
 #--------------------------------------------------------------------------------------------------
-common.plotter <- function(dd, tablename, pwidth, pheight, zposts){
+common.plotter <- function(dd, tablename, pwidth, pheight, zposts, file){
 	N <- length(zposts)-1
-	svglite(file = paste('../tools/plots/time.phases',tablename,'svg',sep='.'), width = pwidth, height = pheight )
-	par(mfrow=c(2,5))
+	svglite(file = file, width = pwidth, height = pheight )
+	ncol <- round(sqrt(N)*1.5)
+	nrow <- ceiling(N/ncol)
+	par(mfrow=c(nrow,ncol))
 	for(n in 1:N){
 		i <- dd$date<=zposts[n] & dd$date>zposts[n+1]
 		data <- dd[i,]
-		main <- paste(n,': ',table,': ',zposts[n],' to ',zposts[n+1],' BP (',zposts[n]-zposts[n+1],' span)',sep='')
+		main <- paste0(n,': ',tablename,': ',zposts[n],' to ',zposts[n+1],' BP (',zposts[n]-zposts[n+1],' span)')
 		plot(NULL,xlim=xlim,ylim=ylim,frame.plot=F,axes=F, xlab='',ylab='',main=main)
 		map('world',xlim=xlim,ylim=ylim,col='grey90',add=T, fill=T, border='grey')
 		points(data$Longitude, data$Latitude, col='steelblue', pch=16,cex=1)
-		#legend('topleft',res$legend,bty='n',col=res$cols,pch=16,cex=0.7)
 		}
 	dev.off()
 	}
+
+choose.zposts <- function(dd){
+	ns <- 1 + floor(log(nrow(dd))*1.5)
+	zposts <- round(rev(quantile(dd$date, prob=seq(0,1,length.out=ns+1))),-2)
+	zposts <- unique(zposts)
+return(zposts)}
 #--------------------------------------------------------------------------------------------------
 # all phases
 #--------------------------------------------------------------------------------------------------
-common.plotter(dd=d, tablename='all', pwidth=pwidth, pheight=pheight, zposts=zposts)
+tablename='all'
+dd <- d
+zposts <- choose.zposts(dd)
+file <- paste0(folder,'/',prefix,'.',tablename,'.svg')
+common.plotter(dd=dd, tablename=tablename, pwidth=pwidth, pheight=pheight, zposts=zposts, file=file)
 #--------------------------------------------------------------------------------------------------
 # Tables directly linked to Phases
 tables <- c('ABotIsotopes','ABotSamples','FaunalBiometrics','FaunalIsotopes','Graves','FaunalSpecies','MaterialCulture')
 #--------------------------------------------------------------------------------------------------
-for(t in 1:length(tables)){
-	tmp <- query.database(paste("SELECT * FROM `BIAD`.`",tables[t],"`",sep=''), conn=conn)
+for(n in 1:length(tables)){
+	tablename <- tables[n]
+	tmp <- query.database(paste("SELECT * FROM `BIAD`.`",tablename,"`",sep=''), conn=conn)
 	dd <- subset(d, PhaseID%in%unique(tmp$PhaseID))
-	common.plotter(dd = dd, tablename=tables[t], pwidth=pwidth, pheight=pheight, zposts=zposts)
+	zposts <- choose.zposts(dd)
+	file <- paste0(folder,'/',prefix,'.',tablename,'.svg')
+	common.plotter(dd=dd, tablename=tablename, pwidth=pwidth, pheight=pheight, zposts=zposts, file=file)
 	}
 #--------------------------------------------------------------------------------------------------
 # More complicated relationships indirectly linked to Phases
@@ -78,14 +96,20 @@ t2 <- query.database("SELECT * FROM `BIAD`.`FaunalIsotopeSequences`", conn=conn)
 t2 <- subset(t2, !is.na(SampleID))
 tmp <- merge(t2, t1, by='SampleID') 
 dd <- subset(d, PhaseID%in%unique(tmp$PhaseID))
-common.plotter(dd = dd, tablename='FaunalIsotopeSequences', pwidth=pwidth, pheight=pheight, zposts=zposts)
+zposts <- choose.zposts(dd)
+tablename <- 'FaunalIsotopeSequences'
+file <- paste0(folder,'/',prefix,'.',tablename,'.svg')
+common.plotter(dd=dd, tablename=tablename, pwidth=pwidth, pheight=pheight, zposts=zposts, file=file)
 #--------------------------------------------------------------------------------------------------
 t1 <- query.database("SELECT * FROM `BIAD`.`Graves`", conn=conn)
 t2 <- query.database("SELECT * FROM `BIAD`.`GraveIndividuals`", conn=conn)
 t2 <- subset(t2, !is.na(GraveID))
 tmp <- merge(t2, t1, by='GraveID')
 dd <- subset(d, PhaseID%in%unique(tmp$PhaseID))
-common.plotter(dd = dd, tablename='GraveIndividuals', pwidth=pwidth, pheight=pheight, zposts=zposts)
+zposts <- choose.zposts(dd)
+tablename <- 'GraveIndividuals'
+file <- paste0(folder,'/',prefix,'.',tablename,'.svg')
+common.plotter(dd=dd, tablename=tablename, pwidth=pwidth, pheight=pheight, zposts=zposts, file=file)
 #--------------------------------------------------------------------------------------------------
 t1 <- query.database("SELECT * FROM `BIAD`.`Graves`", conn=conn)
 t2 <- query.database("SELECT * FROM `BIAD`.`GraveIndividuals`", conn=conn)
@@ -93,7 +117,10 @@ t3 <- query.database("SELECT * FROM `BIAD`.`HumanIsotopes`", conn=conn)
 tmp <- merge(t3, t2, by='IndividualID')
 tmp <- merge(tmp, t1, by='GraveID')
 dd <- subset(d, PhaseID%in%unique(tmp$PhaseID))
-common.plotter(dd = dd, tablename='HumanIsotopes', pwidth=pwidth, pheight=pheight, zposts=zposts)
+zposts <- choose.zposts(dd)
+tablename <- 'HumanIsotopes'
+file <- paste0(folder,'/',prefix,'.',tablename,'.svg')
+common.plotter(dd=dd, tablename=tablename, pwidth=pwidth, pheight=pheight, zposts=zposts, file=file)
 #--------------------------------------------------------------------------------------------------
 t1 <- query.database("SELECT * FROM `BIAD`.`Graves`", conn=conn)
 t2 <- query.database("SELECT * FROM `BIAD`.`GraveIndividuals`", conn=conn)
@@ -101,7 +128,10 @@ t3 <- query.database("SELECT * FROM `BIAD`.`Rites`", conn=conn)
 tmp <- merge(t3, t2, by='IndividualID')
 tmp <- merge(tmp, t1, by='GraveID')
 dd <- subset(d, PhaseID%in%unique(tmp$PhaseID))
-common.plotter(dd = dd, tablename='Rites', pwidth=pwidth, pheight=pheight, zposts=zposts)
+zposts <- choose.zposts(dd)
+tablename <- 'Rites'
+file <- paste0(folder,'/',prefix,'.',tablename,'.svg')
+common.plotter(dd=dd, tablename=tablename, pwidth=pwidth, pheight=pheight, zposts=zposts, file=file)
 #--------------------------------------------------------------------------------------------------
 sql.command <- "SELECT `Sites`.`SiteID`,`Longitude`,`Latitude`,`aDNAID`,`Phases`.`PhaseID` FROM `Sites`
 INNER JOIN `Phases` ON `Sites`.`SiteID`=`Phases`.`SiteID`
@@ -110,7 +140,10 @@ INNER JOIN `GraveIndividuals` ON `GraveIndividuals`.`GraveID`=`Graves`.`GraveID`
 WHERE `GraveIndividuals`.`aDNAID` IS NOT NULL;"
 tmp <- query.database(sql.command, conn=conn)
 dd <- subset(d, PhaseID%in%unique(tmp$PhaseID))
-common.plotter(dd = dd, tablename='aDNA', pwidth=pwidth, pheight=pheight, zposts=zposts)
+zposts <- choose.zposts(dd)
+tablename <- 'aDNA'
+file <- paste0(folder,'/',prefix,'.',tablename,'.svg')
+common.plotter(dd=dd, tablename=tablename, pwidth=pwidth, pheight=pheight, zposts=zposts, file=file)
 #--------------------------------------------------------------------------------------
 disconnect()
 #--------------------------------------------------------------------------------------
